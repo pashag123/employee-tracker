8b4c48ab4a686f41323ea294c0db54962d09677a
pashag123 <pashag123@gmail.com>
2023-11-08 18:49:27 -0800

    joined

--- index.js
+++ index.js
@@ -48,13 +48,13 @@ function startApp() {
                     viewAllRoles()
                     break;
 
-                    case 'view all employees':
+                case 'view all employees':
                     viewAllEmployees()
                     break;
 
 
 
-                case 'Quit':
+                case 'quit':
                     db.close();
 
             }
@@ -85,7 +85,16 @@ async function viewAllRoles() {
 
 async function viewAllEmployees() {
     try {
-        const result = await db.query("select * from employee");
+        const result = await db.query(`SELECT employee.id, employee.first_name AS "first name", employee.last_name 
+        AS "last name", role.title, department.name AS department, role.salary, 
+        concat(manager.first_name, " ", manager.last_name) AS manager
+        FROM employee
+        LEFT JOIN role
+        ON employee.role_id = role.id
+        LEFT JOIN department
+        ON role.department_id = department.id
+        LEFT JOIN employee manager
+        ON manager.id = employee.manager_id`);
         console.table(result)
     } catch (error) {
         console.log(error)
